125ec5668097532d14c136a7c11979a3932d5cd4
andrerpena <andrerpena@gmail.com>
2017-03-01 10:53:53 -0300

    Some TSLint fixes

--- src/server/routes/api.ts
+++ src/server/routes/api.ts
@@ -1,10 +1,8 @@
-import * as express from 'express';
-var apiRouter = express.Router();
+import * as express from "express";
+const apiRouter = express.Router();
 
-apiRouter.route('/test').get((req, res) => {
-    res.status(200).send({
-        text: 'your test has passed - fuck'
-    })
+apiRouter.route("/test").get((req, res) => {
+    res.status(200).send({ text: "your test has passed - fuck" });
 });
 
 export { apiRouter }
