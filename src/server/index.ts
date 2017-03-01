8cc3ffcfc3aeabdc858cc526e49ae4628b8453f9
andrerpena <andrerpena@gmail.com>
2017-02-28 21:38:35 -0300

    Initial commit

---
+++ src/server/index.ts
@@ -0,0 +1,19 @@
+import * as express from 'express';
+import * as colors from 'colors';
+import * as webpack from 'webpack';
+const config = require('../../package.json');
+
+
+// routes
+import { apiRouter } from './routes/api';
+import { appRouter } from './routes/app';
+
+var app = express();
+
+app.use('/api', apiRouter);
+app.use('', appRouter);
+
+
+app.listen(4000, '0.0.0.0', () => {
+    console.log(colors.green(`${config.name} is listening on http://localhost:4000. NODE_ENV: ${process.env.NODE_ENV}`));
+})
